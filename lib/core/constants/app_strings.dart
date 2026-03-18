b42a05181a8bb4ab56905940115673c3c7a68a6e
FootballAnalystAI.github.io <avishkarmenge703@gmail.com>
2026-03-18 21:24:20 +0530

    Create app_strings.dart

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,7 @@
+class AppStrings {
+  static const String appName = 'Oil Shop Manager';
+  static const String scanCoupon = 'Scan Coupon';
+  static const String manualEntry = 'Manual Entry';
+  static const String failedCoupons = 'Failed Coupons';
+  static const String generateReport = 'Generate PDF Report';
+}
